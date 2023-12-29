ee225672e9584f28b8030b3dd0bdfa7610e7b454
a.nizelskiy <a.nizelskiy@axelot.ru>
2023-12-29 17:06:02 +0300

    Create CRUD for Auctions

---
+++ AuctionService/Data/Migrations/20231228195905_ChangeStatusTableName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeStatusTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Auctions",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Auctions",
+                newName: "Type");
+        }
+    }
+}
