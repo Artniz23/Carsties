1599d45a5c041beb5cf9759f698dda42d0872426
a.nizelskiy <a.nizelskiy@axelot.ru>
2024-01-04 21:37:57 +0300

    create connection berween 2 services

---
+++ SearchService/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Entities;
+using SearchService.Services;
+
+namespace SearchService.Data;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb",
+            MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        long count = await DB.CountAsync<Item>();
+
+        using IServiceScope scope = app.Services.CreateScope();
+
+        AuctionServiceHttpClient httpClient = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
+
+        List<Item> items = await httpClient.GetItemsForSearchDb();
+        
+        Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+    }
+}
