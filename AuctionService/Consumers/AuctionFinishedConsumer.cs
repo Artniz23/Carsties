ce460965901216ecfffc6d16ed5f928da7bea8b4
a.nizelskiy <a.nizelskiy@axelot.ru>
2024-01-28 19:42:02 +0300

    Create Gateway Service, Configure Identity Server user for client app

---
+++ AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using AuctionService.Enums;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer(AuctionDbContext dbContext) : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming auction finished");
+        
+        Auction auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction == null) return;
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice ? Status.Finished : Status.ReserveNotMet;
+
+        await dbContext.SaveChangesAsync();
+    }
+}
---
+++ AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer(AuctionDbContext dbContext) : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        Auction auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction == null) return;
+
+        if (
+            auction.CurrentHighBid == null || 
+            context.Message.BidStatus.Contains("Accepted") &&
+            context.Message.Amount > auction.CurrentHighBid
+        )
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
